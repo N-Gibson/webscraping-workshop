465176bac88b463b9170f61328c3d8125f50e89f
Noah Gibson <noah.gibson99@gmail.com>
2019-11-17 16:38:37 -0700

    Fix return values to give titles of all job listings

--- scrape.js
+++ scrape.js
@@ -7,7 +7,7 @@ nightmare
   .click('.icl-WhatWhere-button')
   .wait('.result')
   .evaluate(() => {
-    let jobTitles = document.querySelector('.jobTitle')
+    let jobTitles = document.querySelectorAll('.title')
     let list = [].slice.call(jobTitles)
     return list.map((node) => {
       return node.innerText
